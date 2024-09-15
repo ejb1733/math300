0542cdbcd0fa5055dc9435e7aa5964c5ad8f5780
ejb1733 <ethanjbillings@gmail.com>
2024-09-14 21:03:24 -0700

    add question print function

--- math300code/hw1.py
+++ math300code/hw1.py
@@ -21,9 +21,26 @@ def q1():
     q1dict['q1d'] = (w**2 + w).real
     q1dict['q1e'] = z**2 + z.conjugate() + complex(0,1)
 
+    return q1dict
+
 def q2():
     z = complex(3,5)/complex(1,7)
     w = (complex(-1,np.sqrt(3))/2)**3
 
-q1()
-print(ansDict['Q1'])
+    q2dict = ansDict['Q2']
+
+    q2dict['q2b'] = f'Re(z): {z.real}, Im(z): {round(z.imag,3)}'
+    q2dict['q2c'] = f'Re(w): {w.real}, Im(z): {w.imag}'
+
+    return q2dict
+
+q1dict = q1()
+q2dict = q2()
+
+def printAns():
+    for q in ansDict:
+        print('\n--=--' + q + '--=--\n')
+        for sub_q in ansDict[q]:
+            print(f'{sub_q}: {ansDict[q][sub_q]}')
+
+printAns()
